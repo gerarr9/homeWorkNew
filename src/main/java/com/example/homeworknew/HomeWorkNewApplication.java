4cd45132f0272daf1f92a4712559789395c83036
321ru <gerarr999@yandex.ru>
2023-01-20 14:07:10 +0300

    main

---
+++ src/main/java/com/example/homeworknew/HomeWorkNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworknew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworknew/HomeWorkNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworknew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
